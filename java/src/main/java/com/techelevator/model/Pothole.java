cc2f8d72ba08d248f2e2822eb9c3ad9275b0b463
Said Haji <saidhaji00@gmail.com>
2022-08-09 17:02:03 -0400

    test

--- java/src/main/java/com/techelevator/model/Pothole.java
+++ java/src/main/java/com/techelevator/model/Pothole.java
@@ -152,7 +152,14 @@ public class Pothole {
     }
 
     public void setDescription(String description) {
-        this.description = description.substring(0,200);
+        if(description == null){
+            this.description = description;
+        }
+      else if(description.length() > 200){
+           this.description = description.substring(0,200);
+       }
+       else {this.description = description;};
+
     }
 
     public int getSeverity() {
--- java/src/test/java/com/techelevator/model/PotholeTest.java
+++ java/src/test/java/com/techelevator/model/PotholeTest.java
@@ -2,15 +2,24 @@ package com.techelevator.model;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.security.core.parameters.P;
 
 public class PotholeTest {
 
     @Test
-    public void LongDescriptionIsTruncated(){
+    public void LongDescriptionIsTruncated() {
         String lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
         Pothole pothole = new Pothole();
         pothole.setDescription(lorem);
         Assert.assertEquals(200, pothole.getDescription().length());
 
+    }
+
+    @Test
+    public void AssignShortDescriptionSuccessful() {
+        String lorem = "Lorem Ipsum.";
+        Pothole pothole = new Pothole();
+        pothole.setDescription(lorem);
+        Assert.assertEquals(lorem, pothole.getDescription());
     }
 }
