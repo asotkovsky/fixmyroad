4215513bedaac92691a5b2168052a255e4f121a3
Doug Waxman <waxman.doug@gmail.com>
2022-08-10 11:55:04 -0400

    fixing missing pothole after report bug

--- vue/src/components/ReportForm.vue
+++ vue/src/components/ReportForm.vue
@@ -1,34 +1,69 @@
  <template>
-  <div class='report-attributes'>
-      <div>Road name: {{ newPothole.roadName }}</div>
-      <div> Neighborhood: {{ newPothole.neighborhood }}</div>
-      <div>City name:{{ newPothole.city }}</div>
-      <div>State: {{ newPothole.state }}</div>
-
-  
+  <div class="report-attributes">
+    <div>Road name: {{ $store.state.currentPin.roadName }}</div>
+    <div>Neighborhood: {{ $store.state.currentPin.neighborhood }}</div>
+    <div>City name:{{ $store.state.currentPin.city }}</div>
+    <div>State: {{ $store.state.currentPin.state }}</div>
 
     <form class="report-form" @submit.prevent="handleSave">
-     <label id="severity" for="severity">Severity </label>
-    <div id="severity-selection">
-      <img class='severity-icon' @click="setSeverity(1)" :class="{selected: newPothole.severity == 1}" v-bind:src="require('../assets/severity-icon-1.png')">
-      <img class='severity-icon' @click="setSeverity(2)" :class="{selected: newPothole.severity == 2}" v-bind:src="require('../assets/severity-icon-2.png')">
-      <img class='severity-icon' @click="setSeverity(3)" :class="{selected: newPothole.severity == 3}" v-bind:src="require('../assets/severity-icon-3.png')">
-      <img class='severity-icon' @click="setSeverity(4)" :class="{selected: newPothole.severity == 4}" v-bind:src="require('../assets/severity-icon-4.png')">
-      <img class='severity-icon' @click="setSeverity(5)" :class="{selected: newPothole.severity == 5}" v-bind:src="require('../assets/severity-icon-5.png')">
-    </div>
-  <label id="location_on_roadway" for="location_on_roadway">Location On Roadway</label>
-  <select id="location_on_roadway" v-model="newPothole.locationOnRoadway">
+      <label id="severity" for="severity">Severity </label>
+      <div id="severity-selection">
+        <img
+          class="severity-icon"
+          @click="setSeverity(1)"
+          :class="{ selected: newPothole.severity == 1 }"
+          v-bind:src="require('../assets/severity-icon-1.png')"
+        />
+        <img
+          class="severity-icon"
+          @click="setSeverity(2)"
+          :class="{ selected: newPothole.severity == 2 }"
+          v-bind:src="require('../assets/severity-icon-2.png')"
+        />
+        <img
+          class="severity-icon"
+          @click="setSeverity(3)"
+          :class="{ selected: newPothole.severity == 3 }"
+          v-bind:src="require('../assets/severity-icon-3.png')"
+        />
+        <img
+          class="severity-icon"
+          @click="setSeverity(4)"
+          :class="{ selected: newPothole.severity == 4 }"
+          v-bind:src="require('../assets/severity-icon-4.png')"
+        />
+        <img
+          class="severity-icon"
+          @click="setSeverity(5)"
+          :class="{ selected: newPothole.severity == 5 }"
+          v-bind:src="require('../assets/severity-icon-5.png')"
+        />
+      </div>
+      <label id="location_on_roadway" for="location_on_roadway"
+        >Location On Roadway</label
+      >
+      <select id="location_on_roadway" v-model="newPothole.locationOnRoadway">
+        <option value="road">Road</option>
+        <option value="shoulder">Shoulder</option>
+      </select>
 
-    <option value="road">Road</option>
-    <option value="shoulder">Shoulder</option>
+      <label for="description">Description </label>
+      <textarea
+        id="description"
+        maxlength="200"
+        v-model="newPothole.description"
+        rows="4"
+        cols="50"
+      >
+Please describe the pothole</textarea
+      >
 
-    </select>
-
-    <label for="description">Description </label>
-  <textarea id="description" maxlength = "200" v-model="newPothole.description" rows="4" cols="50">Please describe the pothole</textarea>
-
-    <input type="submit" value="Submit" :disabled="$store.state.currentPin == null || newPothole.severity == 0">
-   </form>
+      <input
+        type="submit"
+        value="Submit"
+        :disabled="$store.state.currentPin == null || newPothole.severity == 0"
+      />
+    </form>
   </div>
 </template>
  
@@ -37,7 +72,7 @@ import PotholeService from "@/services/PotholeService.js";
 
 export default {
   data() {
-    return { 
+    return {
       newPothole: {
         latitude: null,
         longitude: null,
@@ -52,17 +87,23 @@ export default {
     };
   },
   methods: {
-    setSeverity(severitySelection){
+    setSeverity(severitySelection) {
       this.newPothole.severity = severitySelection;
     },
     handleSave() {
-
-      if (this.newPothole.description == ""){
+      if (this.newPothole.description == "") {
         this.newPothole.description = "None provided.";
       }
-      PotholeService.createPothole(this.newPothole).catch(error => {
-        alert(error.message)
-        })
+      this.newPothole.city = this.$store.state.currentPin.city;
+      this.newPothole.state = this.$store.state.currentPin.state;
+      this.newPothole.neighborhood = this.$store.state.currentPin.neighborhood;
+      this.newPothole.roadName = this.$store.state.currentPin.roadName;
+
+      PotholeService.createPothole(this.newPothole)
+        .then(() => this.$router.push({ name: "potholes-list" }))
+        .catch((error) => {
+          alert(error.message);
+        });
 
       // Clear the form for the next addition (and prevents odd bugs in adding data multiple times)
       const newPothole = {
@@ -71,72 +112,32 @@ export default {
         severity: "",
         description: "",
         locationOnRoadway: "",
-        roadName : this.newPothole.roadName, 
-        neighborhood : this.newPothole.neighborhood, 
-        city: this.newPothole.city, 
-        state: this.newPothole.roadName,
+        roadName: this.$store.state.currentPin.roadName,
+        neighborhood: this.$store.state.currentPin.neighborhood,
+        city: this.$store.state.currentPin.city,
+        state: this.$store.state.currentPin.roadName,
       };
-    this.newPothole = newPothole;
-    
-    this.$store.commit("SET_CURRENT_PIN", null)
+      this.newPothole = newPothole;
 
-    this.$router.push("/potholes")
-  }
-  },
-  watch: {
-    "$store.state.currentPin": function () {
-      PotholeService.getLocation(
-        this.$store.state.currentPin.lat,
-        this.$store.state.currentPin.lng
-      ).then((response) => {
-        this.newPothole.latitude = this.$store.state.currentPin.lat;
-        this.newPothole.longitude = this.$store.state.currentPin.lng;
-
-        let addressParts = response.data.results[0].address_components;
-        console.log(addressParts);
-        this.newPothole.city = addressParts.find((part) =>
-          part.types.includes("locality")
-        ).short_name;
-        this.newPothole.roadName = addressParts.find((part) =>
-          part.types.includes("route")
-        ).short_name;
-        let neighborhoodPart = addressParts.find((part) =>
-          part.types.includes("neighborhood")
-        );
-        if (neighborhoodPart) {
-          this.newPothole.neighborhood = neighborhoodPart.short_name;
-        } else this.neighborhood = "";
-
-        this.newPothole.state = addressParts.find((part) =>
-          part.types.includes("administrative_area_level_1")
-        ).short_name;
-      });
+      this.$store.commit("SET_CURRENT_PIN", {});
     },
   },
-}
+};
 </script>
  
  <style>
+img.selected {
+  border-style: solid;
+  border-color: #737373;
+  border-width: 5px;
+}
 
- 
-
-  img.selected {
-    border-style: solid;
-    border-color: #737373;
-    border-width: 5px;
-  }
-
-  #severity-selection {
+#severity-selection {
   display: inline-block;
- 
-  
-  }
+}
 
-
- .report-form {
-   display: flex;
-   flex-direction: column; }
-
-  
-  
+.report-form {
+  display: flex;
+  flex-direction: column;
+}
 </style>
