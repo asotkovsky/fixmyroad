b5b38f333505b7b0dc261d8607e847a86b3be7d2
Doug Waxman <waxman.doug@gmail.com>
2022-08-12 21:28:03 -0400

    default date today in StatusForm

--- vue/src/components/StatusForm.vue
+++ vue/src/components/StatusForm.vue
@@ -2,14 +2,16 @@
   <div>
     <form class="status-form" @submit.prevent="handleSave">
       <select name="status" id="status-select" v-model.number="status.id">
-        <option value="2">
-          Scheduled For Inspection
-        </option>
+        <option value="2">Scheduled For Inspection</option>
         <option value="3">Inspected</option>
         <option value="4">Scheduled For Repair</option>
         <option value="5">Repaired</option>
       </select>
-      <input type="date" v-if="[2,4].includes(status.id)" v-model="status.date"/>
+      <input
+        type="date"
+        v-bind:disabled="![2, 4].includes(status.id)"
+        v-model="status.date"
+      />
       <input type="submit" value="Submit" :disabled="!submitEnabled" />
     </form>
   </div>
@@ -22,8 +24,7 @@ export default {
   name: "status-form",
   data() {
     return {
-      status: {id: null, date: Date()} 
-      
+      status: { id: null, date: new Date().toISOString().slice(0, 10) },
     };
   },
   props: ["pothole"],
@@ -34,14 +35,19 @@ export default {
   },
   methods: {
     handleSave() {
-      PotholeService.createStatus(this.pothole.id, this.status).then(
-        ()=>location.reload()
+      PotholeService.createStatus(this.pothole.id, this.status).then(() =>
+        location.reload()
       );
-      this.status = {id: null, date: Date()};
+      this.status = { id: null, date: new Date().toISOString().slice(0, 10) };
     },
   },
 };
 </script>
 
 <style>
+form.status-form {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
 </style>
